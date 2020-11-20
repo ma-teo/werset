810408dd1e15ee848cc18086d3560c20fb384161
mateo <mateo.tarasiuk@gmail.com>
2020-11-20 15:45:38 +0100

    Add verse choice to session

--- src/app.js
+++ src/app.js
@@ -3,30 +3,30 @@ import Loading from './components/loading'
 import Verse from './components/verse'
 
 const App = () => {
-  const [variables, setVariables] = useState()
-  const [random, setRandom] = useState()
-  const [def, setDef] = useState()
+  const [choice, setChoice] = useState(JSON.parse(sessionStorage.getItem('choice')))
 
-  useEffect(async () => {
+  const handler = async () => {
     const data = await (await fetch(`${process.env.REACT_APP_API_URL}`)).json()
-    setVariables(data)
 
     const book = Math.round(Math.random() * (data.books.length - 1))
     const chapter = Math.round(Math.random() * (data.chapters[book] - 1))
 
     const verses = await (await fetch(`${process.env.REACT_APP_API_URL}/0/${book}/${chapter}`)).json()
-
     const verse = Math.round(Math.random() * (verses.length - 1))
 
-    const choice = verses[verse]
-    setRandom(choice)
-    setDef(`${data.books[book]} ${chapter + 1}:${verse + 1}`)
-  }, [])
+    setChoice({
+      verse: verses[verse],
+      def: `${data.books[book]} ${chapter + 1}:${verse + 1}`
+    })
+  }
+
+  useEffect(() => !sessionStorage.getItem('choice') && handler(), [])
+  useEffect(() => sessionStorage.setItem('choice', JSON.stringify(choice)), [choice])
 
   return (
     <>
       <img className="image" src={`https://picsum.photos/${window.innerWidth}/${window.innerHeight}`} alt="" />
-      {random && def ? <Verse verse={random} def={def} /> : <Loading />}
+      {choice ? <Verse {...choice} /> : <Loading />}
     </>
   )
 }
